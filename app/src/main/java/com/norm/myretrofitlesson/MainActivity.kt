79055730bb2a367ea4636b4e2f8a4d1d64c95656
NORMss <serega.b301@gmail.com>
2024-01-06 20:18:03 +0700

    feat(getAllProducts): added a list with all products
    Closes #4

--- app/src/main/java/com/norm/myretrofitlesson/MainActivity.kt
+++ app/src/main/java/com/norm/myretrofitlesson/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.norm.myretrofitlesson
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.norm.myretrofitlesson.adapter.ProductAdapter
 import com.norm.myretrofitlesson.databinding.ActivityMainBinding
-import com.norm.myretrofitlesson.retrofit.AuthRequest
 import com.norm.myretrofitlesson.retrofit.MainAPI
-import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -16,12 +16,16 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var adapter: ProductAdapter
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        adapter = ProductAdapter()
+        binding.rcView.layoutManager = LinearLayoutManager(this)
+        binding.rcView.adapter = adapter
 
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
@@ -36,20 +40,12 @@ class MainActivity : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         val mainAPI = retrofit.create(MainAPI::class.java)
-        binding.btnSingin.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
-                val user = mainAPI.auth(
-                    AuthRequest(
-                        binding.username.text.toString(),
-                        binding.password.text.toString(),
-                    )
-                )
-                runOnUiThread {
-                    binding.apply {
-                        Picasso.get().load(user.image).into(iv)
-                        firstName.text = user.firstName
-                        lastName.text = user.lastName
-                    }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val list = mainAPI.getAllProducts()
+            runOnUiThread {
+                binding.apply {
+                    adapter.submitList(list.products)
                 }
             }
         }
